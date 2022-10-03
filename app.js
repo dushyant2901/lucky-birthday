18e02a9c2cfb8910b02dc0b443d4aab68cb9daf4
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-10-03 10:37:31 +0530

    add waiting and gif functionality

--- app.js
+++ app.js
@@ -2,32 +2,55 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
-
-function compareValues(sum,luckyNumber){
-  if(sum%luckyNumber===0){
-    outputBox.innerText="Your birthday is lucky 🚀";
-  }else {
-    outputBox.innerText="Your birthday is not lucky 🤒";
+const img = document.querySelector(".img");
+function validateInputs(dob, luckyNum) {
+  if (!dob && !luckyNum) {
+    outputBox.innerText = "Please enter both the fields 😡";
+    return false;
+  } else if (luckyNum <= 0) {
+    outputBox.innerText = "Lucky Number should be greater than zero.";
+    return false;
+  } else {
+    return true;
+  }
+}
+function clearOutput() {
+  outputBox.innerText = "";
+}
+function compareValues(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    img.src = "./img/yes.gif";
+    outputBox.innerText = "Your birthday is lucky 🚀";
+  } else {
+    img.src = "./img/no.gif";
+    outputBox.innerText = "Your birthday is not lucky 🤒";
   }
 }
 
-function checkIfBirthDateIsLucky(){
-  const dob =dateOfBirth.value;
+function checkIfBirthDateIsLucky() {
+  clearOutput();
+  const dob = dateOfBirth.value;
+  const luckyNumberVal = luckyNumber.value;
+  console.log(luckyNumberVal);
+
   const sum = calculateSum(dob);
-  if(sum&&dob)
-  compareValues(sum,luckyNumber.value)
-  else 
-  outputBox.innerText="Please enter both the fields 😡";
+  if (validateInputs(dob, luckyNumberVal)) {
+    img.src = "./img/giphy.gif";
+    checkNumberButton.textContent = "Checking.....";
+    setTimeout(() => {
+      compareValues(sum, luckyNumberVal);
+      checkNumberButton.textContent = "Check Number";
+    }, 2000);
+  }
 }
 
 function calculateSum(dob) {
-  dob= dob.replaceAll("-","");
-  let sum =0;
-  for(let index=0;index<dob.length;index++){
+  dob = dob.replaceAll("-", "");
+  let sum = 0;
+  for (let index = 0; index < dob.length; index++) {
     sum = sum + Number(dob.charAt(index));
   }
   return sum;
 }
 
-checkNumberButton.addEventListener('click',checkIfBirthDateIsLucky)
-
+checkNumberButton.addEventListener("click", checkIfBirthDateIsLucky);
